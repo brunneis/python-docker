e93ed80ee8807b04b95ff3ce6966d442d7e5c5c2
brunneis <dev@brunneis.com>
2018-05-11 10:06:35 +0200

    Python 3.6.5

--- ubuntu/artful/build-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TAGS=(latest 3.6.4 3.6.4-ubuntu 3.6.4-ubuntu17.10 3.6.4-ubuntu-artful)
-
-for tag in ${TAGS[@]}
-    do
-        docker build -t brunneis/python:$tag .
-        docker push brunneis/python:$tag
-    done
--- ubuntu/yakkety/build-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TAGS=(3.6.4-ubuntu16.10 3.6.4-ubuntu-yakkety)
-
-for tag in ${TAGS[@]}
-    do
-        docker build -t brunneis/python:$tag .
-        docker push brunneis/python:$tag
-    done
